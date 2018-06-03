2719b4b9c53cb2a23a37ec3b5d6145a7471affbd
jeffyactive <jeff@reelyactive.com>
2018-06-02 22:04:28 -0400

    Added instruments

---
+++ 4-drums.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright GenieMob 2018
+ * Propulser la créativité | Ignite creativity
+ */
+
+
+let instrument = 4;
+
+
+basic.showString("BATTERIE");
+basic.forever(() => {
+  if(input.acceleration(Dimension.Strength) > 1200) {
+    bluetooth.advertiseUid(49374, instrument, 7, false);
+    basic.showLeds(`
+      . . # . .
+      . # . # .
+      . # . # .
+      . # # # .
+      . # # # .
+    `);
+  }
+  else {
+    bluetooth.stopAdvertising();
+    basic.showLeds(`
+      . . . . .
+      . . . . .
+      . . . . .
+      . . . . .
+      . . . . .
+    `);
+  }
+});
